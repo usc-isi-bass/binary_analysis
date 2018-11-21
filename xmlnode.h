1e437215f3230b719fbd372abf1db1bf6e006335
Christophe Hauser <hauser@isi.edu>
2018-11-21 21:16:22 +0100

    Missing files

---
+++ xmlnode.h
@@ -0,0 +1,23 @@
+struct XMLNode
+{
+    XMLNode(const String &S);
+    ~XMLNode();
+    void FreeMemory();
+
+    void Init(const String &S);
+    void Save(ofstream &File);
+
+    __forceinline void AddChild(XMLNode *Child)
+    {
+        Children.PushEnd(Child);
+    }
+
+    void MakeChildEntryList(Vector<XMLSchemaChildEntry> &List) const;
+    void GetChildrenByName(const String &Name, Vector<XMLNode*> &Result) const;
+    String GetAttributeByName(const String &Name) const;
+
+    String Name;
+    String Value;
+    Vector<XMLAttribute> Attributes;
+    Vector<XMLNode*> Children;
+};
