883d41c2cb7754ec429f0becbaeefc88674a0313
Christophe Hauser <hauser@isi.edu>
2019-03-06 15:02:34 -0800

    Fixed Python 3's relative import stupidity

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,9 +1,17 @@
-from ..analyses import ASTGraph
+import os
+import sys
 import angr
 import logging
 
+# Workaround for stupid Python 3's import system that won't let us to relative
+# imports....
+proj_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+sys.path.append(proj_location)
+from analysis.vex_ast import ASTGraph
+
 l = logging.getLogger("binai.ast").setLevel(logging.DEBUG)
-p = angr.Project("ast-example", auto_load_libs=False)
+binary = os.path.join(proj_location, "binaries/ast-example")
+p = angr.Project(binary, auto_load_libs=False)
 main_addr = p.loader.find_symbol("main").rebased_addr
 
 c = p.analyses.CFGEmulated(keep_state=True)
