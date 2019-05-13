cf40f3c8ee2bed62d3d19ef7df15e8b4bac33038
Shushan Arakelyan <arakelyan.shushan@gmail.com>
2019-05-13 12:52:45 -0700

    small changes in style

--- analysis/binary_analysis_utils.py
+++ analysis/binary_analysis_utils.py
@@ -1,38 +1,15 @@
 import angr 
 import pyvex
 
-
 def node_from_cfg(filename):
     proj = angr.Project(filename, auto_load_libs=False);
     try:
         cfg = proj.analyses.CFG()
     except:
         return    
-
-    main = cfg.kb.functions["main"]
-    node = cfg.get_all_nodes(main.addr)[0]
-
-    processed_nodes = []
-    nodes_to_process = [node]
-    nodes_to_process_addr = set([node.addr])
-    while nodes_to_process:
-        node = nodes_to_process[0]
-        # bookkeeping
-        nodes_to_process = nodes_to_process[1:]
-        nodes_to_process_addr.remove(node.addr)
-        processed_nodes.append(node.addr)
-
-        if node.block is not None:
-            yield node, proj
-
-        successors = cfg.get_all_successors(node)
-        for s in successors:
-            # if the successing node was not visited already or is not 
-            # scheduled for a visit, schedule it
-            if (s.addr not in processed_nodes) and (s.addr not in nodes_to_process_addr):
-                nodes_to_process.append(s)
-                nodes_to_process_addr.add(s.addr)
-
+    for i, n in enumerate(cfg.graph.nodes()):
+        if n.block is not None:
+            yield n, proj
 
 def ir_default_to_string(stmt):
     return stmt.__str__()
