32acc795617aaf085fba7b9b9f6670b211bb7da1
shushan <arakelyan.shushan@gmail.com>
2020-04-20 11:50:05 -0700

    Add experiments 0417_E_pred_MLP_004.py, 0417_E_pred_MLP_005.py, 0417_E_pred_MLP_006.py, 0420_E_pred_GCN_017.py, 0420_E_pred_GCN_018.py, 0420_E_pred_GCN_019.py

---
+++ neural_models/autoencoders/all_experiments/0417_E_pred_MLP_006.py
@@ -0,0 +1,31 @@
+from ..args import Args
+from ..experiment_template import get_experiment_args
+from ..edge_prediction_experiment import main
+
+args = Args()
+
+### CHECK THAT THIS IS A CLEAN REPOSITORY ###
+
+args.EXP_NAME = "E_pred_MLP_006"
+args.EXP_DATE = "0417"
+
+# different_setups
+experiment_setup = {"writer_name": args.EXP_DATE + "_" + args.EXP_NAME,
+                    "writer_comment": None,
+
+                    "batches_log": 100,
+                    "batch_size": 16,
+                    "encoder": "no_encoder",
+                    "epochs_save": 1,
+                    "epochs_test": 1,
+                    "predictor_layer_dims": [5000, 1000, 500, 100],
+                    "undirected_graphs": True,
+
+                    "num_edges": None}
+
+parameters = ["num_edges"]
+values = [(100, ), (500, ), (1000, ), (3000,)]
+
+
+for args in get_experiment_args(args, experiment_setup, parameters, values):
+    main(args)
